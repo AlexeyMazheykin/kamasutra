95d122bb4e324c91758be841e6f2e5f84abc6478
Alexey Mazheykin <mazheykin@icloud.com>
2020-10-02 00:31:21 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,42 +1,19 @@
 import React from 'react';
 import './App.css';
-import logo from './img/logo.png'
+import Content from './components/content';
+import Header from './components/header';
+import Navbar from './components/navbar';
+
+
 
 const App = () => {
   return (
     <div className="App">
-      <header className="header">
-      <div className="header__logo">
-        <img src={logo} alt={"logo"} className="header__logo-pic" />
-      </div>
-      </header>
-      <div className="navbar">
-        <ul className="navbar__list">
-          <li className="navbar__list-item">
-             profile
-          </li>
-          <li className="navbar__list-item">
-             Messages
-          </li>
-          <li className="navbar__list-item">
-             News
-          </li>
-          <li className="navbar__list-item">
-             Music
-          </li>
-          <li className="navbar__list-item">
-             Settings
-          </li>
-        </ul>
-      </div>
-      <div className="content">
-        <div className="content__img">
-          <img className="content__img-pic"src="https://tinypng.com/images/social/website.jpg" />
-        </div>
-        
-      </div>
+      <Header />
+      <Navbar />
+      <Content />
     </div>
-  )
-}
+  );
+};
 
 export default App;
