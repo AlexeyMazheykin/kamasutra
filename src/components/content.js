8abc9c031fea316499fcf83cdfc837baaddcdfca
Alexey Mazheykin <mazheykin@icloud.com>
2020-10-02 00:32:50 +0300

    test

---
+++ src/components/content.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Content = () => {
+    return (
+        <div className="content">
+        <div className="content__img">
+          <img className="content__img-pic" src="https://tinypng.com/images/social/website.jpg" />
+        </div>
+        <div className="">
+          ava + desc
+        </div>
+        <div className="">
+          My posts
+          </div>
+      </div>
+    );
+}
+export default Content;
---
+++ src/components/navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+const Navbar = () => {
+    return (
+      <div className="navbar">
+      <ul className="navbar__list">
+        <li className="navbar__list-item">
+           profile
+        </li>
+        <li className="navbar__list-item">
+           Messages
+        </li>
+        <li className="navbar__list-item">
+           News
+        </li>
+        <li className="navbar__list-item">
+           Music
+        </li>
+        <li className="navbar__list-item">
+           Settings
+        </li>
+      </ul>
+    </div>
+    );
+        
+}
+export default Navbar;
